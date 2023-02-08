25e615d1ff3964d9e74a717d48d830e37e769856
BetterLogicTeam <102576102+BetterLogicTeam@users.noreply.github.com>
2023-02-08 20:08:20 +0500

    First Commit

---
+++ src/Component/Lockestak/Lockestake.jsx
@@ -0,0 +1,243 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Web3 from "web3";
+import {
+  TokenAddress,
+  Token_Abi,
+  tokenStaking,
+  tokenStaking_Abi,
+} from "../../utilies/constant";
+import Connent from "../Connent/Connent";
+import "./Lockestake.css";
+import Countdown from "react-countdown";
+import moment from "moment/moment";
+import { Button, Popover } from "antd";
+import { Modal, Space } from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+import axios from "axios";
+
+function Lockestake({ setShoww, check }) {
+  let { provider, acc, providerType, web3 } = useSelector(
+    (state) => state.connectWallet
+  );
+  const [selectDays, setselectDays] = useState(0);
+  const [getValue, setgetValue] = useState(0);
+  const [Active, setActive] = useState(0);
+  const [spinner, setspinner] = useState(false);
+  const [balance, setbalance] = useState(0);
+  const [selectedCard, setselectedCard] = useState(null);
+  const [cradShow, setcradShow] = useState([]);
+  const [stakeddata, setstakeddata] = useState();
+  const [cardIndex, setcardIndex] = useState([]);
+  const [slectedAllnfton, setslectedAllnfton] = useState({
+    condition: false,
+    walletOfOwneron: [],
+  });
+  const [noSelectedAll, setnoSelectedAll] = useState([]);
+
+  const staking_Amount = async () => {
+    try {
+      if (acc == null) {
+        toast.error("Please Connect Metamaske First!");
+        setShoww(true);
+      } else {
+        if (getValue == null) {
+          toast.error("Please Enter Amount First!");
+          setspinner(false);
+        } else {
+          if (getValue < 100) {
+            toast.error("Minimum Staking Amount 100!");
+            setspinner(false);
+          } else {
+            if (selectDays == 0) {
+              toast.error("Please Select Days");
+              setspinner(false);
+            } else {
+              if(balance>=getValue){
+
+              
+              setspinner(true);
+              let stakingContractOf;
+              let tokenContractOf;
+
+              stakingContractOf = new web3.eth.Contract(
+                tokenStaking_Abi,
+                tokenStaking
+              );
+              tokenContractOf = new web3.eth.Contract(Token_Abi, TokenAddress);
+
+              let stakingValue = web3.utils.toWei(getValue);
+
+              // console.log("stakingValue", stakingValue);
+
+              // await tokenContractOf.methods
+              //   .approve(tokenStaking, stakingValue)
+              //   .send({
+              //     from: acc,
+              //   });
+              // toast.success("Approve Confirmed");
+              await stakingContractOf.methods.farm(selectDays).send({
+                from: acc,
+                value: stakingValue,
+              });
+              toast.success("Transaction Confirmed");
+              setspinner(false);
+            }else{
+              toast.error("Insufficient Balance")
+            }
+            }
+          }
+        }
+      }
+    } catch (e) {
+      console.log("Error", e);
+      setspinner(false);
+    }
+  };
+  const checkBalance = async () => {
+    const webSupply = new Web3("https://bsc-testnet.public.blastapi.io");
+
+    let tokenContractOf = new webSupply.eth.Contract(Token_Abi, TokenAddress);
+    let stakingContractOf = new webSupply.eth.Contract(
+      tokenStaking_Abi,
+      tokenStaking
+    );
+
+    if (acc != null) {
+      web3.eth.getBalance(acc.toString(), function (err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("data", web3.utils.fromWei(result, "ether"));
+
+          let balanceOf = web3.utils.fromWei(result, "ether");
+          setbalance(parseFloat(balanceOf).toFixed(3));
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    checkBalance();
+  });
+
+  return (
+    <>
+      <>
+        <div className="container-fluid p-0  ">
+          <div className="row justify-content-center">
+            <div className="col-lg-5 all_main p-0">
+              <h3 class="staking__selector__heading">Stake ARC</h3>
+
+              <div className="first_box mt-4  px-2">
+                <div className="munt_box d-flex justify-content-between">
+                  <span className="">Amount</span>
+                  <p className="my_balnc ">
+                    <span> ~My balance:</span> <span>{balance} </span>
+                  </p>
+                </div>
+                <div className="typ_area border ">
+                  <div className="mx_buttn str_tp_dollar text-cenetr ">
+                    ARC
+                  </div>
+                  <input
+                    className="ariia"
+                    type="number"
+                    inputMode="decimal"
+                    placeholder="0"
+                    autoComplete="off"
+                    autoCorrect="off"
+                    aria-aria-valuemin="0"
+                    aria-valuemax="9007199254740991"
+                    onChange={(e) => setgetValue(e.target.value)}
+                    value={getValue}
+                  />
+
+                  <button
+                    type="button"
+                    className="mx_buttn text-white "
+                    onClick={() => setgetValue(balance)}
+                  >
+                    Max
+                  </button>
+                </div>
+              </div>
+
+              <div className="second_box mt-3 px-2">
+                <p className="text-start">Locking Time</p>
+                <div className="time_table">
+                  <div className="dan_gtr text-white">
+                    <div
+                      className=" border des_tw p-0 "
+                      style={{
+                        background: Active == 1 ? "#b311f3" : "transparent",
+                      }}
+                      onClick={() => (setselectDays(30), setActive(1))}
+                    >
+                      <button className="btn btn-md dates">30 Days</button>
+                      <div className="arp border-top">14% ARP</div>
+                    </div>
+                    <div
+                      className=" border des_tw p-0"
+                      style={{
+                        background: Active == 2 ? "#b311f3" : "transparent",
+                      }}
+                      onClick={() => (setselectDays(90), setActive(2))}
+                    >
+                      <button className="btn btn-md dates">90 Days</button>
+                      <div className="arp border-top">17% ARP</div>
+                    </div>
+                    <div
+                      className=" border des_tw p-0"
+                      style={{
+                        background: Active == 3 ? "#b311f3" : "transparent",
+                      }}
+                      onClick={() => (setselectDays(180), setActive(3))}
+                    >
+                      <button className="btn btn-md dates">180 Days</button>
+                      <div className="arp border-top">20% ARP</div>
+                    </div>
+                    <div
+                      className=" border des_tw p-0"
+                      style={{
+                        background: Active == 4 ? "#b311f3" : "transparent",
+                      }}
+                      onClick={() => (setselectDays(360), setActive(4))}
+                    >
+                      <button className="btn btn-md dates">360 Days</button>
+                      <div className="arp border-top">25% ARP</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <button
+                className="btn btn-md lst_btnn mt-3 text-white"
+                onClick={() => staking_Amount()}
+              >
+                {spinner == true ? (
+                  <>
+                    <div class="spinner-border" role="status">
+                      <span class="visually-hidden">Loading...</span>
+                    </div>
+                  </>
+                ) : (
+                  " Enable Staking"
+                )}
+              </button>
+
+              <div className="last mt-4">
+                <p className="fon m-0 py-2">
+                  Locking {getValue} ARC for {selectDays} Days
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    </>
+  );
+}
+
+export default Lockestake;
---
+++ src/utilies/constant.js
@@ -0,0 +1,238 @@
+
+
+export const tokenStaking = "0x256E1a54e32177Fe026e2e83Ef84E4f9F181269d";
+export const tokenStaking_Abi =[{"type":"constructor","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256[]","name":"","internalType":"uint256[]"},{"type":"uint256[]","name":"","internalType":"uint256[]"},{"type":"uint256[]","name":"","internalType":"uint256[]"}],"name":"UserInformation","inputs":[{"type":"address","name":"_add","internalType":"address"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"DepositeToken","internalType":"uint256"},{"type":"uint256","name":"lastUpdated","internalType":"uint256"},{"type":"uint256","name":"lockableDays","internalType":"uint256"},{"type":"uint256","name":"WithdrawReward","internalType":"uint256"},{"type":"uint256","name":"WithdrawAbleReward","internalType":"uint256"},{"type":"uint256","name":"depositeTime","internalType":"uint256"},{"type":"uint256","name":"WithdrawDepositeAmount","internalType":"uint256"}],"name":"Users","inputs":[{"type":"address","name":"","internalType":"address"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"allocation","inputs":[{"type":"uint256","name":"","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"changeDeductionPercentage","inputs":[{"type":"uint256","name":"amount","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"changeMinimmumAmount","inputs":[{"type":"uint256","name":"amount","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"changePercentages","inputs":[{"type":"uint256","name":"_30dayspercent","internalType":"uint256"},{"type":"uint256","name":"_90dayspercent","internalType":"uint256"},{"type":"uint256","name":"_180dayspercent","internalType":"uint256"},{"type":"uint256","name":"_360dayspercent","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"changetimeCal","inputs":[{"type":"uint256","name":"_time","internalType":"uint256"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"depositeToken","inputs":[{"type":"address","name":"","internalType":"address"},{"type":"uint256","name":"","internalType":"uint256"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"depositetime","inputs":[{"type":"address","name":"","internalType":"address"},{"type":"uint256","name":"","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"emergencyWithdraw","inputs":[{"type":"address","name":"_token","internalType":"contract IARC20"},{"type":"uint256","name":"_amount","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"emergencyWithdrawARC","inputs":[{"type":"uint256","name":"Amount","internalType":"uint256"}]},{"type":"function","stateMutability":"payable","outputs":[],"name":"farm","inputs":[{"type":"uint256","name":"_lockableDays","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"harvest","inputs":[{"type":"uint256[]","name":"_index","internalType":"uint256[]"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"lockabledays","inputs":[{"type":"address","name":"","internalType":"address"},{"type":"uint256","name":"","internalType":"uint256"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"address","name":"","internalType":"address"}],"name":"owner","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"reward","internalType":"uint256"}],"name":"pendindRewards","inputs":[{"type":"address","name":"_add","internalType":"address"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"transferOwnership","inputs":[{"type":"address","name":"newOwner","internalType":"address"}]},{"type":"event","name":"Deposite_","inputs":[{"type":"address","name":"to","indexed":true},{"type":"address","name":"From","indexed":true},{"type":"uint256","name":"amount","indexed":false},{"type":"uint256","name":"day","indexed":false},{"type":"uint256","name":"time","indexed":false}],"anonymous":false},{"type":"event","name":"OwnershipTransferred","inputs":[{"type":"address","name":"previousOwner","indexed":true},{"type":"address","name":"newOwner","indexed":true}],"anonymous":false},{"type":"receive"}]
+
+export const TokenAddress = "0x208F521710620d417E9f35a37f107e360f4A7c3d"
+export const Token_Abi = [{
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+}, {
+    "anonymous": false,
+    "inputs": [{
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+    }, {
+        "indexed": true,
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+    }, {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+    }],
+    "name": "Approval",
+    "type": "event"
+}, {
+    "anonymous": false,
+    "inputs": [{
+        "indexed": true,
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+    }, {
+        "indexed": true,
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+    }, {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+    }],
+    "name": "Transfer",
+    "type": "event"
+}, {
+    "inputs": [{
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+    }],
+    "name": "Mint",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+    }, {
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+    }],
+    "name": "allowance",
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+    }, {
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+    }],
+    "name": "approve",
+    "outputs": [{
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+    }],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+    }],
+    "name": "balanceOf",
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{
+        "internalType": "uint256",
+        "name": "_value",
+        "type": "uint256"
+    }],
+    "name": "burn",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [{
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+    }, {
+        "internalType": "uint256",
+        "name": "subtractedValue",
+        "type": "uint256"
+    }],
+    "name": "decreaseAllowance",
+    "outputs": [{
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+    }],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+    }, {
+        "internalType": "uint256",
+        "name": "addedValue",
+        "type": "uint256"
+    }],
+    "name": "increaseAllowance",
+    "outputs": [{
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+    }],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "name",
+    "outputs": [{
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [{
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [{
+        "internalType": "address",
+        "name": "recipient",
+        "type": "address"
+    }, {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+    }],
+    "name": "transfer",
+    "outputs": [{
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+    }],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{
+        "internalType": "address",
+        "name": "sender",
+        "type": "address"
+    }, {
+        "internalType": "address",
+        "name": "recipient",
+        "type": "address"
+    }, {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+    }],
+    "name": "transferFrom",
+    "outputs": [{
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+    }],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}]
+
